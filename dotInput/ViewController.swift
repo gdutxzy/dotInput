49d19e2e681954a681472f1721e43e194e1876f3
XZY <xiezongyuan@xhg.com>
2018-10-23 10:38:18 +0800

    Initial Commit

---
+++ dotInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dotInput
+//
+//  Created by XZY on 2018/10/23.
+//  Copyright © 2018年 xiezongyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
